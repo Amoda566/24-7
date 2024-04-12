a00e97050c506104f306ab1d687ccd54e4d1b1e1
Amoda566 <91647261+Amoda566@users.noreply.github.com>
2024-04-13 03:36:03 +0530

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("24/7 Staizer Music",{ type: 'STREMING'});
+  await client.user.setActivity("24/7 Staizer Music",{ type: 'Listening'});
 };
